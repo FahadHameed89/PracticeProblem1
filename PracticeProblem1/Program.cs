7ffc10193fd33fac1309ad2535e7a5c6ca2404bb
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-08 20:27:30 -0800

    Created PracticeProblem1 on Github

---
+++ PracticeProblem1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
